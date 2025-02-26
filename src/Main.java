334d131ecc25f2903e77d1746286d7f5c4e395df
marcelosftelles <marcelosftelles@gmail.com>
2025-02-26 04:24:24 -0300

    Arquivos iniciais de Estudos

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        String name = "Marcelo";
+        String adress = "Rua fulaninho da silva, PJ";
+        double salary = 1950.50;
+        String dateReceived = "21 de janeiro de 2021";
+        /* Da pra fazer todo o texto do println numa string */
+        System.out.println("Eu "+name+", morando na cidade de "+adress+", confirmo que recebi o salário");
+    }
+}
---
+++ src/TiposPrimitivos.java
@@ -0,0 +1,15 @@
+public class TiposPrimitivos {
+    public static void main(String[] args) {
+        int age = 21;
+        long bigNumber = 210000;
+        double salaryDouble = 2100;
+        float salaryFloat = 2100.21F;
+        byte ageByte = 21;
+        short ageShort = 21;
+        boolean verdadeiro = true;
+        char caracter = 'M';
+        String name = "É O TELLES !";
+
+
+    }
+}
